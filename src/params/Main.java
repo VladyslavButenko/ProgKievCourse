d50529f206e76992fab8bcf73dce355263b5b089
Vladyslav Butenko <vbutenko713@gmail.com>
2018-10-23 19:10:43 +0300

    Task 3-7

--- src/params/Main.java
+++ src/params/Main.java
@@ -3,7 +3,7 @@ package params;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "  pa5ram1=value1&param2=va8lue2&param3=value3";
+        String url = "pa5ram1=value1&param2=va8lue2&param3=value3";
         String[] paramsAndValues = url.split("&");
         for (String str : paramsAndValues) {
             if (!str.matches("[A-z0-9]+[0-9]=[A-z0-9]+[0-9](&|$)")) {
